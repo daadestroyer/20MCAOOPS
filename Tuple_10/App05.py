883e82c6cc0845923dd7a09f9908619d0af4cb34
daadestroyer <nigamshubham2000@gmail.com>
2021-01-24 16:01:19 +0530

    loop tuples completed...

---
+++ Tuple_10/App05.py
@@ -0,0 +1,17 @@
+# Loop tuples
+tpl = ('apple', 'mango', 'orange', 'banana', 'cherry')
+for x in tpl:
+    print(x)
+
+# Loop through the index no
+print()
+
+for i in range(len(tpl)):
+    print(tpl[i])
+
+# using while loop
+print()
+i =0
+while i<len(tpl):
+    print(tpl[i])
+    i+=1
