d39c94d9253caec6adb7b8af842d522ef8333b7f
daadestroyer <nigamshubham2000@gmail.com>
2021-01-24 09:50:07 +0530

    copy list completed...

--- List_06/App06.py
+++ List_06/App06.py
@@ -5,3 +5,32 @@ for x in fruits:
     newlist.append(x.upper())
 
 print(newlist)
+
+# sort list alphanumerically
+fruits.sort()
+print(fruits)
+
+# sort list numerically
+nos = [4,2,5,1,6,3]
+nos.sort()
+print(nos)
+
+# sort descending order
+nos.sort(reverse=True)
+print(nos)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+thislist.reverse()
+print(thislist)
+thislist.sort(reverse=True)
+print(thislist)
+
+
+# python copy list
+
+originallist = [1,2,3,4,5]
+copylist = originallist.copy()
+print(copylist)
