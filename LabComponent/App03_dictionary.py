39dfe484cf9cad65afb8f1237491a50dc77f5dff
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2021-06-13 16:37:44 +0530

    Update App03_dictionary.py

--- LabComponent/App03_dictionary.py
+++ LabComponent/App03_dictionary.py
@@ -1,66 +1,64 @@
 d = dict()
 class employee:
-    def input(self):
-        self.name = input("enter emp name\n")
-        self.address = input("enter address\n")
-        self.pan = input("enter pan details\n")
-        self.basic = int(input("enter basic salary\n"))
-        self.tds = int(input("enter tds\n"))
-        self.deduct = int(input("enter deductions\n"))
-        self.hra = 1.25*self.basic
-        self.da = 0.25*self.basic
-        self.gross_sal = self.basic+self.hra+self.da
-        self.net_pay = self.gross_sal-self.deduct
-        self.addemp()
+		def input(self):
+			self.name = input('enter name : ')
+			self.addr = input('enter addr : ')
+			self.pan = input('enter pan : ')
+			self.basic = int(input('enter basic : '))
+			self.tds = int(input('enter tds : '))
+			self.deduct = int(input('enter decuct : '))
+			self.hra = 1.25*self.basic
+			self.da = 0.25*self.basic
+			self.grosssal = self.basic + self.hra + self.da
+			self.netpay = self.grosssal-self.deduct
+			self.addemp()
 
-    def addemp(self):
-        d.update(
-            {
-               self.name: {
-               "name": self.name,
-               "address": self.address,
-               "pan": self.pan,
-               "basic": self.basic,
-               "tds": self.tds,
-               "deduct": self.deduct,
-               "hra": self.hra,
-               "da": self.da,
-               "gross_sal": self.gross_sal,
-               "net_pay": self.net_pay
-               }
-            }
-        )
+		def addemp(self):
+			d.update({
+					self.name:{
+							"name":self.name,
+							"addr":self.addr,
+							"pan":self.pan,
+							"basic":self.basic,
+							"tds":self.tds,
+							"deduct":self.deduct,
+							"hra":self.hra,
+							"da":self.da,
+							"grosssal":self.grosssal,
+							"netpay":self.netpay
+						}
+				})
 
-    def search(self, name):
-        flag = 0
-        for key in d:
-            if key == name:
-                print("employee found")
-                for i in d[key]:
-                    print(i, d[key][i])
-                    flag = 1
-        if flag == 0:
-            print("no record found")
+		def printallemp(self):
+			for key in d:
+				print(key,d[key])
 
-    def printallemp(self):
-        for key in d:
-            print(key, d[key])
+		def search(self,name):
+			flag = 0
+			for key in d:
+				if name == key:
+					print('employee found')
+					for i in d[key]:
+						print(i,d[key][i])
+						flag = 1
+				if flag == 0:
+					print('no record found')
 
+class operation:
+	emp = employee()
 
-class operations(employee):
-    em = employee()
-    while True:
-        ch = int(input(
-            "\n 1 to add employee or employees \n "
-            "   2 - print all employees \n "
-            "   3 - find an employee by name \n 0-exit \n"))
-        if ch == 1:
-            em.input()
-        elif ch == 2:
-            em.printallemp()
-        elif ch == 3:
-            name = input("enter the name of the employee to be searched")
-            em.search(name)
-        else:
-            break
-    print("thank you for using employee mangement system")
+	while True:
+		print('1. to add employee or employees : ')
+		print('2. print all employees : ')
+		print('3.find an employee by name : ')
+		choice = int(input('enter choice : '))
+
+		if choice == 1:
+			emp.input()
+		elif choice == 2:
+			emp.printallemp()
+		elif choice == 3:
+			name = input('enter employee name : ')
+			emp.search(name)
+		else:
+			print('*** EXITING ***')
