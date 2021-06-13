af069b52a50ccb8ebae543355730e8a15448ba30
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2021-06-14 00:03:04 +0530

    Update and rename App10.py to App10_generator.py

--- LabComponent/App10_generator.py
+++ LabComponent/App10_generator.py
@@ -23,3 +23,26 @@ f = fib()
 num = int(input("enter range for series"))
 for i in range(num):
     print(next(f))
+    
+    
+
+# def fib(limit):	
+# 	a, b = 0, 1
+
+# 	while a < limit:
+# 		yield a
+# 		a,b=b,a+b
+
+# x = fib(5)
+
+# # try:
+# #     while True:
+# #         print(x.__next__())
+# # except StopIteration:
+# #     print('STOP ITERATING')
+    
+# for i in x:
+#     print(i)
+
+
+
