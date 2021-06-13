9c03d993ed41f4ee4fc441329ba99d4a8a7adcf6
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2021-06-13 09:10:00 +0530

    Update App01_a_Implement10OperationsOnStrings.py

--- LabComponent/App01_a_Implement10OperationsOnStrings.py
+++ LabComponent/App01_a_Implement10OperationsOnStrings.py
@@ -1,58 +1,51 @@
-# take a string input from user
+while(True):
+	print('1. convert to lower case')
+	print('2. convert to upper case')
+	print('3. length of string')
+	print('4. reverse string')
+	print('5. concat two string')
+	print('6. get first character of string')
+	print('7. get last character of string')
+	print('8. get substring')
+	print('9. repeat string twice')
+	print('10. check character present in string or not')
+	print('0. EXIT')
+	print('')
+	choice = int(input('enter your choice : '))
 
-
-while (True):
-    print('1. convert string toUpperCase')
-    print('2. convert string toLowerCase')
-    print('3. find length of string')
-    print('4. reverse the string')
-    print('5. concat two strings')
-    print('6. get first character of string')
-    print('7. get last character of string')
-    print('8. check string is palindrome or not')
-    print('9. sort the string')
-    print('10. search word in a string')
-    print('11. EXIT')
-    print()
-    choice = int(input('Enter choice : '))
-
-    if choice == 1:
-        str1 = input('Enter string 1 : ')
-        print(str1.upper())
-    elif choice == 2:
-        str1 = input('Enter string 1 : ')
-        print(str1.lower())
-    elif choice == 3:
-        str1 = input('Enter string 1 : ')
-        print(len(str1))
-    elif choice == 4:
-        str1 = input('Enter string 1 : ')
-        print(str1[::-1])
-    elif choice == 5:
-        str1 = input('Enter string 1 : ')
-        str2 = input('Enter string 2 : ')
-        print(str1+" "+str2)
-    elif choice == 6:
-        str1 = input('Enter string 1 : ')
-        print(str1[0])
-    elif choice == 7:
-        str1 = input('Enter string 1 : ')
-        print(str[-1])
-    elif choice == 8:
-        str1 = input('Enter string 1 : ')
-        str2 = str1[::-1]
-        if str1 == str2:
-            print('Palindrome')
-        else:
-            print("Not Palindrome")
-    elif choice == 9:
-        str1 = input('Enter string 1 : ')
-        print(sorted(str1))
-    elif choice == 10:
-        str1 = input('Enter string 1 : ')
-        search = input("Enter word/character to search in a string : ")
-        print(str1.find(search))
-    else:
-        break;
-
-print("EXITED...")
+	if choice == 1:
+		str = input('enter string : ')
+		print(str.lower())
+	elif choice == 2:
+		str = input('enter string : ')
+		print(str.upper())
+	elif choice == 3:
+		str = input('enter string : ')
+		print(len(str))
+	elif choice == 4:
+		str = input('enter string : ')
+		print(str[::-1])
+	elif choice == 5:
+		str1 = input('enter 1st string : ')
+		str2 = input('enter 2nd string : ')
+		print(str1+str2)
+	elif choice == 6:
+		str = input('enter string : ')
+		print(str[0])
+	elif choice == 7:
+		str = input('enter string : ')
+		print(str[-1])
+	elif choice == 8:
+		str = input('enter string : ')
+		print(str[2:4])
+	elif choice == 9:
+		str = input('enter string : ')
+		print(str*2)
+	elif choice == 10:
+		str = input('enter string : ')
+		char = input('enter character to check : ')
+		print(char in str)
+	else :
+		print('')
+		print('*** EXITING ***')
+		break
